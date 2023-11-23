7c13176a177a3e6300446f4f5a42f31237459270
u3609489 <146449839+u3609489@users.noreply.github.com>
2023-11-23 18:01:43 +0800

    Update onehavenocards.h

--- onehavenocards.h
+++ onehavenocards.h
@@ -3,6 +3,10 @@
 #ifndef ONE_HAVE_NO_CARDS_H
 #define ONE_HAVE_NO_CARDS_H
 
+// Defining new type of variable "UNO"
+// For example,
+// ( UNO.col == 'n' && UNO.num == 'D' ) is Draw 4
+// ( UNO.col == 'b' && UNO.num == '7' ) is Blue 7
 struct UNO {
     char col;
     char num;
