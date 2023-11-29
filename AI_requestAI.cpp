4e1a550302734340e0e4a71881ce0815cf22ea2b
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-29 22:44:56 +0800

    Update AI_requestAI.cpp
    The case of 'n' has been added

--- AI_requestAI.cpp
+++ AI_requestAI.cpp
@@ -36,9 +36,18 @@ playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ //
 				player[AIIndex].card[j] = player[AIIndex].card[j + 1]; // remove the card and move the cards to the front
 			}
 		}
-	else{ // when the card is 'n'
-		
-		
+	else{ // decision making after the wild card, we have to check colorTochange not the card color
+		for (int i = 0; i < 100 ; i++){
+			if (player[AIIndex].card[i].col == currentCard.colorToChange){ // when the card.col is same as colorTochange 
+				currentCard = player[AIIndex].card[i]; // change the current card
+				pos = i;
+				found = true;
+				break;
+			}
+			if(player[AIIndedx].card[i] == 0){ // when there is no card anymore
+				break;
+			}
+		} // in this case we dont need to check the num of card. cuz we have to put the correspond color card.
 	}
 	return currentCard;
 }
@@ -62,7 +71,7 @@ playedUNO AI_requestAI(ListOfCards player[AIIndex], int handsize, playedUNO &cur
 			currentCard = easyAI(player[], AIIndex, currentCard);
 			break;
 		case 'n':
-			
+			currentCard = easyAI(player[], AIIndex, currentCard);
 	}
 	return currentCard;
 }
