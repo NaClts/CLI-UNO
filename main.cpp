6ea3bb407a9eedc707b39f5bda730efa311bd880
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-29 22:55:56 +0800

    Update main.cpp
    AI_requestAI() parameter has been updated

--- main.cpp
+++ main.cpp
@@ -95,9 +95,8 @@ void startNewGame() {
             currentCard = display_requestUser(player[0], currentCard);    // The card played by player is stored as "currentCard"
         else {
             int AIIndex = counter % *numOfPlayers;    // The turn of AI
-            int handsize = sizeof(player[AIIndex].card) / sizeof(player[AIIndex].card[0]); // calculate array size of player[AIIndex].card
             display_waitingForAI(currentCard, AIIndex, player[0]); // Display which AI is playing and wait for a time delay of 1 second
-            currentCard = AI_requestAI(player[AIIndex], currentCard); // The card played by AI is stored as "currentCard"
+            currentCard = AI_requestAI(player[], AIIndex ,currentCard); // The card played by AI is stored as "currentCard"
         }
 
         ///////////////////////////////////////////
