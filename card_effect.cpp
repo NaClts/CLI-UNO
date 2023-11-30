3f499b15137d067bda04d1f84c2ebb4116b4e668
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-30 11:31:52 +0800

    Update card_effect.cpp
    randomSingleUNO(time(NULL)) function has been changed

--- card_effect.cpp
+++ card_effect.cpp
@@ -17,7 +17,7 @@ void skip(int &counter, bool reverse){ // skip the next player's turn
 void Draw2(ListOfCards &player[], int AIIndex){ // adding 2 cards to the player
 	UNO add_card1, add_card2;
 	add_card1 = randomSingleUNO(time(NULL)); // random 2 cards from the deck
-	add_card2 = randomSingleUNO(time(NULL));
+	add_card2 = randomSingleUNO(time(NULL)+1);
 	for(int i = 0; i < 100 ; i++){ // add the random card at the back of the array
 		if(player[AIIndex].card[i] == 0){
 			player[AIIndex].card[i] = add_card1;
@@ -51,9 +51,9 @@ void Wild(playedUNO &currentCard){ //change the color
 void WildDraw(ListOfCards &player[], int AIIndex){// adding 4 cards to the player 
 	UNO add_card1, add_card2, add_card3, add_card4;
 	add_card1 = randomSingleUNO(time(NULL)); // generate 4 random cards
-	add_card2 = randomSingleUNO(time(NULL));
-	add_card3 = randomSingleUNO(time(NULL));
-	add_card4 = randomSingleUNO(time(NULL));
+	add_card2 = randomSingleUNO(time(NULL)+1);
+	add_card3 = randomSingleUNO(time(NULL)+2);
+	add_card4 = randomSingleUNO(time(NULL)+3);
 	for(int i = 0; i < 100 ; i++){ // add the random card at the back of the array
 		if(player[AIIndex].card[i] == 0){
 			player[AIIndex].card[i] = add_card1;
