070be3ba2fd63374f24f3c2a1f7b0b99d73b3fef
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-30 13:02:16 +0800

    Create display_result.cpp
    display_result.cpp is uploaded

---
+++ display_result.cpp
@@ -0,0 +1,23 @@
+// display_result.cpp
+
+#include <iostream>
+#include "selfDefStruct.h"
+#include "whoWin.h"
+
+void display_result(ListOfCards player[], int numOfPlayer) {
+    int winner = whoWin(player, numOfPlayer);
+    if (winner == -1) {
+        std::cout << "The game is still ongoing.\n";
+    } else if (winner == 0) {
+        std::cout << "The human player has won the game!\n";
+    } else {
+        std::cout << "AI #" << winner << " has won the game!\n";
+    }
+
+    // Display the final state of the cards
+    for (int i = 0; i < numOfPlayer; i++) {
+        std::cout << "Player " << i << " has " << getNumOfCards(player[i]) << " cards left.\n";
+    }
+}
+
+
