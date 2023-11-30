86e6d6b71e60f0746148ea79ac0eb7e8cc59a791
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-30 13:54:36 +0800

    Update AI_requestAI.cpp
    necessary header file and library added

--- AI_requestAI.cpp
+++ AI_requestAI.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 #include "selfDefStruct.h"
-
+#include "random.h"
 
 playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ // AI mode is defined based on how the AI search for what card to put
 	// easyAI search the array of card and find the first occurrence of appropriate card
