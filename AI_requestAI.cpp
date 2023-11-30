71e68a564467d662b1b22bafdfd1a2d082f3b52e
Baek Seunghyeon <146543232+dae9999nam@users.noreply.github.com>
2023-11-30 12:51:57 +0800

    Update AI_requestAI.cpp
    Bug fix

--- AI_requestAI.cpp
+++ AI_requestAI.cpp
@@ -6,6 +6,7 @@ playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ //
 	// easyAI search the array of card and find the first occurrence of appropriate card
 	int pos = 0;
 	bool found = false;
+	
 	if(currentCard.card.col != 'n'){ // check the card and when it is not 'n'
 		for (int i = 0; i < 100 ; i++){
 			if (player[AIIndex].card[i].col == currentCard.card.col){ // when the card.col is same as currentCard  
@@ -14,7 +15,7 @@ playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ //
 				found = true;
 				break;
 			}
-			if(player[AIIndedx].card[i] == 0){ // when there is no card anymore
+			if(player[AIIndedx].card[i].col){ // when there is no card anymore // when the col is empty, there is no card
 				break;
 			}
 		}
@@ -26,15 +27,17 @@ playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ //
 					found = true;
 					break;
 				}
-				if(player[AIIndex].card[i] == 0){ // when there is no more card
+				if(player[AIIndex].card[i].col){ // when there is no more card
 					break;
 				}
 			}
 		}
 		if(found == true){
-			for (int j = pos; j < handsize ; j++){ // only for when the player put the card
+			for (int j = pos; j < 99 ; j++){ // only for when the player put the card
 				player[AIIndex].card[j] = player[AIIndex].card[j + 1]; // remove the card and move the cards to the front
 			}
+			player[AIIndex].card[99].col = '\0';
+			player[AIIndex].card[99].num = '\0';
 		}
 	else{ // decision making after the wild card, we have to check colorTochange not the card color
 		for (int i = 0; i < 100 ; i++){
@@ -44,10 +47,17 @@ playedUNO easyAI(ListOfCards &player[], int AIIndex, playedUNO &currentCard){ //
 				found = true;
 				break;
 			}
-			if(player[AIIndedx].card[i] == 0){ // when there is no card anymore
+			if(player[AIIndedx].card[i].col){ // when there is no card anymore
 				break;
 			}
 		} // in this case we dont need to check the num of card. cuz we have to put the correspond color card.
+		if(found == true){ // when the AI put the card, it should be removed from the hand
+			for (int j = pos; j < 99 ; j++){ // only for when the player put the card
+				player[AIIndex].card[j] = player[AIIndex].card[j + 1]; // remove the card and move the cards to the front
+			}
+			player[AIIndex].card[99].col = '\0';
+			player[AIIndex].card[99].num = '\0';
+		}
 	}
 	return currentCard;
 }
